e1a188a39755c9802adf3dd1ac449577390b1d0f
ansons <ansonstewart@gmail.com>
2019-01-07 21:04:10 -0500

    test-case: remove trips

---
+++ Scripts/Modification/Boston/Remove Trips/35 outbound remove/Script1546912820969.groovy
@@ -0,0 +1,28 @@
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Modification/Shared/Create Modification'), [('modName') : '35 outbound remove', ('modType') : 'Remove Trips'], 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Modification/Shared/Set Feed'), [('feedName') : 'MBTA'], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Modification/Shared/Set Route after Feed'), [('routeName') : '35 Dedham'], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Modification/Shared/Set Pattern'), [('patternName') : 'from Forest'], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Modification/Shared/Set Pattern'), [('patternName') : 'from Forest'], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Modification/Back to Modifications'))
+
