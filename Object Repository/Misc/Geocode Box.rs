12e2f0e4af2c7ecbb4285376a7a268962c8a924d
ansons <ansonstewart@gmail.com>
2019-08-08 11:29:25 -0400

    test-case: geocode

---
+++ Object Repository/Misc/Geocode Box.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Geocode Box</name>
+   <tag></tag>
+   <elementGuidId>fa74902c-81b5-48bb-9cce-2d76e9ad5170</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class[contains(.,'placeholder')] and (text() = 'Search map...')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div[class$=control] input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
